c56feb8cdd93f8cab2739a99dc30d5acbc4e53e4
bianquinhamaiza <maiza.guilherme@estudante.ifms.edu.br>
2023-08-31 10:19:43 -0400

    atualizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
-import Header from './header/Header'
-import Footer from './footer/Footer'
+import Header from './header/Header.tsx'
+import Footer from './footer/Footer.tsx'
+import Main from './main/Main.tsx'
 function App() {
   return (<>
   <Header/>
+  <Main/>
   <Footer/>
   </>
   )
---
+++ src/main/Main.tsx
@@ -0,0 +1,12 @@
+import Filme from "../filme/Filme"
+
+export default function Main(){
+    return(
+        <main className="content-main">
+            <Filme titulo="Barbiezinha" 
+            sinopse="jasdbhdidALKDMaldkjisjlkfnsdakjfbskjfksjbfdksnfd"
+            imagem="filmebarbie.jpg"/>
+            <Filme/>
+        </main>
+    )
+}
